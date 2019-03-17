26f397515ec64bb3bcd8b20d6ac9d1432d1f8661
VadimKutovoi <sir.kutovoi@yandex.ru>
2019-03-17 19:34:28 +0300

    Added rehash function realisation

--- SimplePerfectHash/SimplePerfectHash/SUH.h
+++ SimplePerfectHash/SimplePerfectHash/SUH.h
@@ -16,13 +16,14 @@ private:
     uint prime;
     uint alpha;
     uint betta;
-    std::vector<int> table;
+    uint table_size;
     static std::mt19937 gen;
     static std::uniform_int_distribution<uint> random;
 public:
-    uhash(unsigned int table_size);
+    uhash(uint _table_size);
     uint hash(int key, int alpha, int betta);
     void rehash();
+    uint add();
 };
 
 uhash::uhash(uint _table_size) {
@@ -34,7 +35,6 @@ std::mt19937 uhash::gen(time(0));
 std::uniform_int_distribution<uint> uhash::random(0, UINT32_MAX);
 
 uint uhash::hash(int key, int alpha, int betta) {
-    uint table_size = table.size;
     if (table_size == 1)
         return 0;
     return (alpha * key + betta) % prime % table_size;
@@ -42,6 +42,8 @@ uint uhash::hash(int key, int alpha, int betta) {
 
 void uhash::rehash() {
     alpha = random(gen) % (prime - 1);
+    if (alpha == 0) ++alpha;
+    betta = random(gen) % (betta - 1);
 }
 
 #endif // !SUH
