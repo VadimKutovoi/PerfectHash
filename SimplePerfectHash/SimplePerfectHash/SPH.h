f60d7e279ab4f037af495a350e69bfef32853619
VadimKutovoi <sir.kutovoi@yandex.ru>
2019-03-19 22:31:03 +0300

    Added simple testing main

---
+++ SimplePerfectHash/SimplePerfectHash/SPH.h
@@ -0,0 +1,17 @@
+#include "SUH.h"
+
+#ifndef SIMPLE_PERFECT_HASH
+#define SIMPLE_PERFECT_HASH
+
+class perfhash 
+{
+private:
+public:
+    perfhash();
+};
+
+perfhash::perfhash() {
+
+}
+
+#endif
--- SimplePerfectHash/SimplePerfectHash/testmain.cpp
+++ SimplePerfectHash/SimplePerfectHash/testmain.cpp
@@ -1,25 +1,30 @@
 #include "SUH.h"
 #include <iostream>
 
-#define HASH_TABLE_SIZE 10
-#define DATA_VECTOR_SIZE 5
+#define HASH_TABLE_SIZE 15
+#define DATA_VECTOR_SIZE 10
 
 int main() {
-    std::vector<uint> test_table(HASH_TABLE_SIZE);
     std::vector<int> test_data;
     unihash uh(HASH_TABLE_SIZE);
 
-    for (int i = 0; i <= DATA_VECTOR_SIZE; ++i)
-        test_data.push_back(i);
+    for (int i = 0; i < DATA_VECTOR_SIZE; ++i)
+        test_data.push_back(i + 1);
 
-    for (int i = 0; i <= DATA_VECTOR_SIZE; ++i)
+    for (int i = 0; i < DATA_VECTOR_SIZE; ++i)
         std::cout << test_data[i] << std::endl;
 
     uh.rehash();
-    for (int i = 0; i <= DATA_VECTOR_SIZE; ++i)
-        test_table.insert(test_table.begin() + uh.hash(test_data[i]), test_data[i]);
+    uh.build_table(test_data);
 
     std::cout << "Hashing results" << std::endl;
-    for (int i = 0; i <= HASH_TABLE_SIZE; ++i)
-        std::cout << test_table[i] << std::endl;
+    for (int i = 0; i < HASH_TABLE_SIZE; ++i)
+        std::cout << uh.get_cell(i) << std::endl;
+
+    std::cout << "Comparing results..." << std::endl;
+
+    for (int i = 0; i < DATA_VECTOR_SIZE; ++i) {
+        int item = uh.find(test_data[i]);
+        std::cout << item << std::endl;
+    }
 }
